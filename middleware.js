4c36e9dbc1bc56fba62a4ae4de5b1c78efd6d856
Farad Alam <farad.dev@gmail.com>
2025-04-16 22:16:31 +0600

    display text acording to localization or prefered text

--- middleware.js
+++ middleware.js
@@ -36,3 +36,13 @@ export async function middleware(request) {
 
   return NextResponse.next();
 }
+
+// Solution of conflict tailwind css
+// export const config = {
+//   matcher: [
+//     // Skip all internal paths (_next, assets, api)
+//     "/((?!api|assets|.*\\..*|_next).*)",
+//     // Optional: only run on root (/) URL
+//     // '/'
+//   ],
+// };
